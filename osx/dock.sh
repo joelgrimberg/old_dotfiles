6939e6bd783b3783f9ef0d72531ab145b80e3474
joelgrimberg <joel@MAC0515445.local>
2016-12-08 08:41:25 +0100

    added dock apps

--- osx/dock.sh
+++ osx/dock.sh
@@ -1,4 +1,46 @@
 ## setting up the dock
+#Resetting Dock to default settings
+defaults delete com.apple.dock; killall Dock
+
+#Airmail3.app
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key>
+<string>/Applications/Agenda.app</string><key>_CFURLStringType</key>
+<integer>0</integer></dict></dict></dict>'
+
+#Agenda.app
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key>
+<string>/Applications/Agenda.app</string><key>_CFURLStringType</key>
+<integer>0</integer></dict></dict></dict>'
+
+#Agenda.app
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key>
+<string>/Applications/Agenda.app</string><key>_CFURLStringType</key>
+<integer>0</integer></dict></dict></dict>'
+
+#WhatsApp.app
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key>
+<string>/Applications/WhatsApp.app</string><key>_CFURLStringType</key>
+<integer>0</integer></dict></dict></dict>'
+
+#Twitter.app
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key>
+<string>/Applications/Twitter.app</string><key>_CFURLStringType</key>
+<integer>0</integer></dict></dict></dict>'
+
+#OmniFocus.app
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key>
+<string>/Applications/OmniFocus.app</string><key>_CFURLStringType</key>
+<integer>0</integer></dict></dict></dict>'
+
+#TogglDesktop.app
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key>
+<string>/Applications/TogglDesktop.app</string><key>_CFURLStringType</key>
+<integer>0</integer></dict></dict></dict>'
+
+#Slack.app
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key>
+<string>/Applications/Slack.app</string><key>_CFURLStringType</key>
+<integer>0</integer></dict></dict></dict>'
 
 #Mindnode.app
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key>
@@ -10,4 +52,3 @@ defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</
 
 #restart Dock
 killall Dock
-
